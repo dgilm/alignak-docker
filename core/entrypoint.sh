71d1b0e93ab3256d1264ef4f488b9a300ab17849
David Gil <dgil@a3sec.com>
2019-09-19 14:06:49 +0200

    install packages via pip and connect core with backend

--- core/entrypoint.sh
+++ core/entrypoint.sh
@@ -7,4 +7,20 @@ mkdir -p /usr/local/var/log/alignak \
 mkdir -p /usr/local/var/run/alignak \
     && chown -R alignak:alignak /usr/local/var/run/alignak
 
-/usr/bin/supervisord
+sed -i \
+    -e 's/;modules=backend_broker, logs/modules=backend_broker/' \
+    -e 's/;modules=backend_scheduler/modules=backend_scheduler/' \
+    -e 's/;modules=backend_arbiter/modules=backend_arbiter/' \
+    /usr/local/share/alignak/etc/alignak.d/daemons.ini
+
+sed -i \
+    's#api_url=http://127.0.0.1:5000#api_url=http://backend:5000#' \
+    /usr/local/share/alignak/etc/alignak.d/backend-broker.ini
+sed -i \
+    's#api_url=http://127.0.0.1:5000#api_url=http://backend:5000#' \
+    /usr/local/share/alignak/etc/alignak.d/backend-arbiter.ini
+sed -i \
+    's#api_url=http://127.0.0.1:5000#api_url=http://backend:5000#' \
+    /usr/local/share/alignak/etc/alignak.d/backend-scheduler.ini
+
+/usr/bin/supervisord -c /etc/supervisor/conf.d/alignak.conf
